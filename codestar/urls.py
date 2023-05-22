169799fb18e37c4766c1a9794816bfb486c7ca8d
Nikkeljohn <nikkeljohn120@gmail.com>
2023-05-22 11:53:30 +0000

    linked about and contact pages

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,4 +20,6 @@ from hotel import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
+    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='about'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
-# Create your views here.
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
