46a5fec1c77153be8eb3193504b3b61f89b957ce
Nikkeljohn <nikkeljohn120@gmail.com>
2023-05-22 15:03:20 +0000

    added css style

--- hotel/models.py
+++ hotel/models.py
@@ -17,3 +17,28 @@ class Contact(models.Model):
         verbose_name_plural = "Contact Table"
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profiles/%Y/%m/%d', null=True, blank=True)
+    contact_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name
+
+    class Meta:
+        verbose_name_plural="Profile Table"
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="team")
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    # facebook_url = models.CharField(blank=True,max_length=200)
+    # twitter_url = models.CharField(blank=True,max_length=200)
+
+    def __str__(self):
+        return self.name 
+
