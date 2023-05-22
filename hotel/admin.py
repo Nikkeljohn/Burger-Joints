e56fb548a0b5e1551ec2c952c189ba8a26fbfa81
Nikkeljohn <nikkeljohn120@gmail.com>
2023-05-22 12:57:14 +0000

    user can send message from contact page to admin

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from hotel.models import Contact
 
 # Register your models here.
+
+admin.site.site_header = "BurgerJoint | Admin"
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id','name','email','subject','added_on','is_approved']
+
+admin.site.register(Contact, ContactAdmin)
--- hotel/views.py
+++ hotel/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from hotel import Contact
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
-def contact(request):
-    return render(request, 'contact.html')
+def contact_us(request):
+    context={}
+    if request.method=="POST":
+        name = request.POST.get("name")
+        em = request.POST.get("email")
+        sub = request.POST.get("subject")
+        msz = request.POST.get("message")
+        
+        obj = Contact(name=name, email=em, subject=sub, message=msz)
+        obj.save()
+        context['message']=f"Dear {name}, Your Mesage has been sent!"
+        
+   
+    return render(request,'contact.html', context)
 
 def about(request):
     return render(request, 'about.html')
