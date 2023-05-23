fd067374a8ebda6526dc33c32cdb7e752c8b7fb2
Nikkeljohn <nikkeljohn120@gmail.com>
2023-05-23 06:50:01 +0000

    .gitignore is now working

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"]="postgres://iscphdpk:NxnIKsZo1Y8C-GnXj4-mRpBbj_dP5UHP@horton.db.elephantsql.com/iscphdpk"
-os.environ["SECRET_KEY"]="9495314785nj"
-os.environ["CLOUDINARY_URL"]="cloudinary://684588912737568:ch_qoxfZlKQcYLspyD09mEcfko4@duloytybb"
