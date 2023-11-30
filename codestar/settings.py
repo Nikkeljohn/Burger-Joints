9bf09c7f35f78f08e6330c7bf9079aecbb2e8f75
Nikkeljohn <nikkeljohn120@gmail.com>
2023-11-30 21:20:05 +0000

    Change the settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -6,7 +6,7 @@ if os.path.exists('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
+#TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
