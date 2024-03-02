effbd8bbc3e4d0406178a01c0d7529c8d9988b47
Nikkeljohn <nikkeljohn120@gmail.com>
2024-03-02 14:54:01 +0000

    add product details and views

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,31 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views 
-from django.conf import settings 
+from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/',views.contact_us,name="contact"),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    
-    
-    
-    
-
-    # path('about/',views.about,name="about"),
-    # path('team/',views.team_members,name="team"),
-    # path('dishes/',views.all_dishes,name="all_dishes"),
-    # path('register/',views.register,name="register"),
-    # path('check_user_exists/',views.check_user_exists,name="check_user_exist"),
-    # path('login/', views.signin, name='login'),
-    # path('dashboard/', views.dashboard, name='dashboard'),
-    # path('logout/', views.user_logout, name='logout'),
-    # path('dish/<int:id>/', views.single_dish, name='dish'),
-    # path('menu/', views.menu,name='menu'),    
-    # path('dishes/',views.all_dishes,name="all_dishes"),
-    # path('dish/<int:id>/', views.single_dish, name='dish'),
-    # path('delete_dish/<slug:pk>/', views.DeleteDish.as_view(),name='delete_dish')
+    path('products/', include('products.urls')),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
