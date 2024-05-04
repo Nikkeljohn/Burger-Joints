ad18eddfc3b31a719bbb61529fb96805182b16ec
Nikkeljohn <nikkeljohn120@gmail.com>
2024-05-04 16:06:36 +0000

    did migrations

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-from home.models import Contact
+from home.models import Contact 
 from django.http import HttpResponse,JsonResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
@@ -15,11 +15,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def menu(request):
-    return render(request, 'hotel/menu.html')
 
-def about(request):
-    return render(request, 'hotel/about.html')
     
 def index(request):
     
@@ -27,76 +23,19 @@ def index(request):
 
 @login_required
 def contact_us(request):
-    context={}
-    if request.method=="POST":
+    context = {}
+    if request.method == "POST":
         name = request.POST.get("name")
         em = request.POST.get("email")
         sub = request.POST.get("subject")
         msz = request.POST.get("message")
-        
         obj = Contact(name=name, email=em, subject=sub, message=msz)
         obj.save()
-        context['message']=f"Dear {name}, Thanks for your time!"
+        context['message'] = f"Dear {name}, Thanks for your time!"
 
     return render(request, 'home/contact.html', context)
 
-def about(request):
-    return render(request, 'hotel/about.html')
 
-def team_members(request):
-    context={}
-    members = Team.objects.all().order_by('name')
-    context['team_members'] = members
-    return render(request, 'hotel/team.html', context)
-
-
-def register(request):
-    context={}
-    if request.method=="POST":
-        #fetch data from html form
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        password = request.POST.get('pass')
-        contact = request.POST.get('number')
-        check = User.objects.filter(username=email)
-        if len(check)==0:
-            #Save data to both tables
-            usr = User.objects.create_user(email, email, password)
-            usr.first_name = name
-            usr.save()
-
-            profile = Profile(user=usr, contact_number = contact)
-            profile.save()
-            context['status'] = f"User {name} Registered Successfully!"
-        else:
-            context['error'] = f"A User with this email already exists"
-
-    return render(request, 'home/register.html', context)
-
-def check_user_exists(request):
-    email = request.GET.get('usern')
-    check = User.objects.filter(username=email)
-    if len(check)==0:
-        return JsonResponse({'status':0,'message':'Not Exist'})
-    else:
-        return JsonResponse({'status':1,'message':'A user with this email already exists!'})
-
-def signin(request):
-    context={}
-    if request.method=="POST":
-        email = request.POST.get('email')
-        passw = request.POST.get('password')
-
-        check_user = authenticate(username=email, password=passw)
-        if check_user:
-            login(request, check_user)
-            if check_user.is_superuser or check_user.is_staff:
-                return HttpResponseRedirect('/admin')
-            return HttpResponseRedirect('/dashboard')
-        else:
-            context.update({'message':'Invalid Login Details!','class':'alert-danger'})
-
-    return render(request,'home/login.html', context)
 
 def dashboard(request):
     context={}
@@ -143,8 +82,4 @@ def dashboard(request):
     #context['orders']=orders   
 
     return render(request, 'home/dashboard.html')
-
-
-
-
 
