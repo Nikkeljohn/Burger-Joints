c3d5ff2f643fa155953bd3cffa617bd42d24e958
Nikkeljohn <nikkeljohn120@gmail.com>
2024-05-20 12:43:20 +0000

    edit profile models, views and templayes

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+admin.site.site_header = "BurgerJoint | Admin"
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id','name','email','subject','added_on','is_approved']
+
+
+
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ['id','name','price','added_on','updated_on']
+
+
+admin.site.register(Contact, ContactAdmin)
